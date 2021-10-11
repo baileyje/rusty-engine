60e5994af661836349a1e4d0b646283e24080f6a
John Bailey <baileyje@gmail.com>
2021-10-11 11:02:19 -0500

    feat: Some restructuring to better align thread mechanics.

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,11 +1,17 @@
-use engine::core::Core;
-use engine::core::service::Service;
+use engine::core::{Engine, Service};
 
 fn main() {
-  
-  let core = Core::new();
-  let service_one = Service::new(String::from("First Service"));
-  core.add(service_one);
-  core.start().expect("Failed to start core");
-  
+  let service_one = Service::new("First Service".into());
+  Engine::new()
+    .add(service_one)
+    .start(
+      String::from("foo"),
+      |frame, data| {
+        println!("Update: {} -> {}", frame.time.as_millis(), data);
+      },
+      |frame, data| {
+        println!("Fixed Update: {} -> {}", frame.fixed_time.as_millis(), data);
+      },
+    )
+    .expect("Failed to start core");
 }
